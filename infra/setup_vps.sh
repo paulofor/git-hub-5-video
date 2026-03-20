1bcfd0115c614deee642f217a2e63e1b15dfcbe9
paulofor <paulofore@gmail.com>
2026-03-20 10:50:56 -0300

    chore: padroniza DB para aihubvideodb_user e aihubvideodb

--- infra/setup_vps.sh
+++ infra/setup_vps.sh
@@ -335,8 +335,8 @@ EOF
 
   echo
   echo "Configurando banco de dados MySQL provisionado na hospedagem"
-  DB_URL="jdbc:mysql://d555d.vps-kinghost.net:3306/aihubdb"
-  DB_USER="aihubdb_user"
+  DB_URL="jdbc:mysql://d555d.vps-kinghost.net:3306/aihubvideodb"
+  DB_USER="aihubvideodb_user"
   DB_PASS="A9m#Q2v@T7x%L4n*Z6p+H3c&B8d-K1r5J"
 
   echo
@@ -417,7 +417,7 @@ Configuração concluída! Principais informações:
 - Frontend publicado na porta: ${FRONTEND_HTTP_PORT}
 - Backend publicado na porta: ${BACKEND_HTTP_PORT}
 - Sandbox publicado na porta: ${SANDBOX_ORCHESTRATOR_HTTP_PORT}
-- Banco de dados externo: jdbc:mysql://d555d.vps-kinghost.net:3306/aihubdb
+- Banco de dados externo: jdbc:mysql://d555d.vps-kinghost.net:3306/aihubvideodb
 - Chave privada do deploy: ${DEPLOY_KEY_PATH}
 - authorized_keys atualizado em: ${HOME}/.ssh/authorized_keys
 
